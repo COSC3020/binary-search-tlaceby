464efe068243bc5ae8384b0b759a922274d73b26
tlaceby <tylerbadgertutorials@gmail.com>
2024-01-24 17:05:44 -0700

    Final

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
-function binarySearch(list, element) {
-    return -1;
+
+function binarySearch(list = [], element) {
+    if (list.length == 0) return -1;
+    if (list.length == 1) 
+        return list[0] === element ? 0 : -1;
+    
+    const pivot = Math.floor(list.length / 2);
+    const at = list[pivot];
+
+    if (at === element) return pivot;
+    else if (at > element) 
+        return binarySearch(list.slice(0, pivot), element);
+
+    return binarySearch(list.slice(pivot + 1, list.length), element);
 }
